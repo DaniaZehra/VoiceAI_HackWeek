59fb7b94d8f4b668ec7b03adc6cc1f1560997bc0
DaniaZehra <daniazehra332@gmail.com>
2025-10-05 20:03:40 +0500

    resolving an error

--- voice-pos-app-frontend/src/components/Dashboard.tsx
+++ voice-pos-app-frontend/src/components/Dashboard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { FaChartBar, FaBoxOpen, FaReceipt, FaBoxes } from 'react-icons/fa';
+// import { FaChartBar, FaBoxOpen, FaReceipt, FaBoxes } from 'react-icons/fa';
 import { ReactNode } from 'react';
 
 interface DashboardCardProps {
